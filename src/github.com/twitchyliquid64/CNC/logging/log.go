22daf0e9841bc907330061db53f0025f3c46c673
Tom <xxx@xxx>
2015-11-14 12:53:07 +1100

    Added colors to logging

--- src/github.com/twitchyliquid64/CNC/logging/log.go
+++ src/github.com/twitchyliquid64/CNC/logging/log.go
@@ -9,33 +9,41 @@ import (
 
 
 func Info(module string, content ...interface{}) {
-	writeLogLine(formatLogPrefix(module, "I", content...))
+	writeLogLine(formatLogPrefix(module, "I", cyan(), content...))
+		publishLogMessage("I", module, content...)
 }
 
 func Warning(module string, content ...interface{}) {
-	writeLogLine(formatLogPrefix(module, "W", content...))
+	writeLogLine(formatLogPrefix(module, "W", yellow(), content...))
+		publishLogMessage("W", module, content...)
 }
 
 func Error(module string, content ...interface{}) {
-	writeLogLine(formatLogPrefix(module, "E", content...))
+	writeLogLine(formatLogPrefix(module, "E", red(), content...))
+		publishLogMessage("E", module, content...)
 }
 
 func Fatal(module string, content ...interface{}) {
-	writeLogLine(formatLogPrefix(module, "F", content...))
+	writeLogLine(formatLogPrefix(module, "F", red(), content...))
+	publishLogMessage("F", module, content...)
 	os.Exit(1)
 }
 
 
-func formatLogPrefix(module, messagePrefix string, content ...interface{})string {
+func formatLogPrefix(module, messagePrefix, prefixColor string, content ...interface{})string {
 	c := fmt.Sprint(content...)
 	module = strings.ToUpper(module)
 	if module != "" {
-		return "[" + messagePrefix + "] [" + module + "] " + c
+		return prefixColor + "[" + messagePrefix + "] " + blue() + "[" + module + "] " + clear() + c
 	} else {
-		return "[" + messagePrefix + "] " + c
+		return prefixColor + "[" + messagePrefix + "] " + clear() + c
 	}
 }
 
 func writeLogLine(inp string) {
 	log.Println(inp)
+}
+
+func publishLogMessage(msgType, module string, content ...interface{}) {
+
 }
