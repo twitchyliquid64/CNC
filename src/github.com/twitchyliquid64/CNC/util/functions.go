5f3ca2d010844d8a69fbfdcf960ddb9ed2aedf6c
jableader <jacobdunk@gmail.com>
2015-12-13 19:31:20 +1100

    Fixed logging

--- src/github.com/twitchyliquid64/CNC/util/functions.go
+++ src/github.com/twitchyliquid64/CNC/util/functions.go
@@ -11,11 +11,11 @@ func GetFunc(argument otto.Value, vm *otto.Otto) otto.Value {
   }
 
   mname := argument.String()
-  logging.Warning("Using a string to name parameters is now depricated. Please use callbacks directly: " + mname)
+  logging.Warning("plugin-builtin", "Using a string to name parameters is now deprecated. Please use callbacks directly: " + mname)
 
   method, err := vm.Get(mname)
   if (err != nil){
-    logging.Error("Error getting function \"" + mname + "\": ")
+    logging.Error("plugin-builtin", "Error getting function \"" + mname + "\": ")
   }
 
   return method;
